888af312dde9948e20ed390cc348a9c1a5382cbc
youngs2170 <youngs2170@student.faytechcc.edu>
2024-11-24 19:40:55 -0500

    Add files via upload

---
+++ P2LAB1.py
@@ -0,0 +1,50 @@
+# Young, Shandi
+
+# 12 Nov, 2024
+
+# P1HW2
+
+# Circumference of a circle
+
+#
+
+#
+
+#------pseudocode---
+
+#display
+
+#input radius
+
+#set area = diameter circumference
+
+#
+
+
+
+
+#input radius of circle
+radius = float(input('What is the radius of the circle?'))
+
+print()
+
+#import math for pi measurements
+import math
+
+diameter =radius * 2
+
+circumference = math.pi * radius * 2
+
+area = radius =math.pi * (radius**2)
+
+
+#auto print measurements 
+print('The diameter of the circle is', diameter)
+
+print()
+
+print(f"The circumference of the circle is: {circumference:.2f}")
+
+print()
+
+print(f"The area of the circle is: {area:.3f}")
